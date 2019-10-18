17e98092c70e48efbdf9f4ffae96945c8e7e1b40
Miguel A.S. Tabadero <48539976+sirmiggles@users.noreply.github.com>
2019-10-18 18:03:23 +0800

    Added basic MPI functions

--- src/fwd.c
+++ src/fwd.c
@@ -19,49 +19,65 @@
 
 int main(int argc, char** argv) {
     //  Print usage if there are no arguments provided
-    if (argc < 2) {
-        printUsage();
-        return -1;
+
+    int rank, clusterSize;
+    //MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &clusterSize);
+    switch (rank) {
+        case 0:
+            if (argc < 2) {
+                printUsage();
+                return -1;
+            }
+
+            char* fileName = parseFileName(argv[1]);
+            if (!fileName) {
+                return -1;
+            }
+
+            int* inputData = fileToPointer(fileName);
+            if (!inputData) {
+                return -1;
+            }
+
+            int numV = inputData[0];
+            int* edgeArray = malloc(sizeof(int) * numV * numV);
+            if (!edgeArray) {
+                fprintf(stderr, "Error: could not allocate memory to edgeArray @ %s\n", __func__);
+                return -1;
+            }
+
+            memcpy(edgeArray, inputData + 1, sizeof(int) * numV * numV);
+
+            free(inputData);
+
+            int** adjMatrix = convertTo2DMatrix(numV, edgeArray);
+            if (!adjMatrix) {
+                return -1;
+            }
+
+            struct timeval start, end;
+            gettimeofday(&start, NULL);
+            int** distances = initDistances(numV, adjMatrix);
+            if (!distances) {
+                return -1;
+            }
+            
+            distances = floydWarshall(numV, adjMatrix, distances);
+            gettimeofday(&end, NULL);
+            double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+            printf("Executed in %10.6f\n", delta);
+            free(distances);
+            free(adjMatrix);
+            break;
+        
+        default: 
+            printf("Bro man, I'm %d\n", rank);
     }
 
-    char* fileName = parseFileName(argv[1]);
-    if (!fileName) {
-        return -1;
-    }
-
-    int* inputData = fileToPointer(fileName);
-    if (!inputData) {
-        return -1;
-    }
-
-    int numV = inputData[0];
-    int* edgeArray = malloc(sizeof(int) * numV * numV);
-    if (!edgeArray) {
-        fprintf(stderr, "Error: could not allocate memory to edgeArray @ %s\n", __func__);
-        return -1;
-    }
-
-    memcpy(edgeArray, inputData + 1, sizeof(int) * numV * numV);
-
-    free(inputData);
-
-    int** adjMatrix = convertTo2DMatrix(numV, edgeArray);
-    if (!adjMatrix) {
-        return -1;
-    }
-
-    struct timeval start, end;
-    gettimeofday(&start, NULL);
-    int** distances = initDistances(numV, adjMatrix);
-    if (!distances) {
-        return -1;
-    }
-    
-    distances = floydWarshall(numV, adjMatrix, distances);
-    gettimeofday(&end, NULL);
-    double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
-    printf("Executed in %10.6f\n", delta);
-    free(distances);
-    free(adjMatrix);
+    MPI_Finalize();
     return 0;
 }
