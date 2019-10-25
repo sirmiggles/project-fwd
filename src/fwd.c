58300f63ea56e478898f9b816b254e13ac92ea2a
Miguel A.S. Tabadero <48539976+sirmiggles@users.noreply.github.com>
2019-10-25 08:11:05 +0800

    Added file output determination

--- src/fwd.c
+++ src/fwd.c
@@ -12,6 +12,7 @@
 #include <sys/time.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <time.h>
 
 #include <mpi.h>
 
@@ -27,6 +28,10 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &clusterSize);
 
+    time_t now;
+    struct tm *execStart;
+    char outFileName[32];
+
     //  Head Node File I/O
     if (rank == 0) {
         if (argc < 2) {
@@ -36,13 +41,20 @@ int main(int argc, char** argv) {
 
         char* fileName = parseFileName(argv[1]);
         if (!fileName) {
+            MPI_Finalize();
             return -1;
         }
 
         int* inputData = fileToPointer(fileName);
         if (!inputData) {
+            MPI_Finalize();
             return -1;
         }
+
+        //  Initialize outfile params
+        now = time(NULL);
+        execStart = localtime(&now);
+        strftime(outFileName, 32, "%Y-%m-%d_%H%M.out", execStart);
 
         numV = inputData[0];
         printf("Number of vertices in graph: %d\n", numV);
@@ -71,16 +83,25 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    int** matrix = (int**) malloc(sizeof(int) * numV * numV);
+    int** matrix = (int**) malloc(sizeof(int*) * numV);
+    for (int i = 0; i < numV; i++) {
+        if(!(matrix[i] = (int*) malloc(sizeof(int*) * numV))) {
+            fprintf(stderr, "Error: could not allocate memory to matrix @ %s\n", __func__);
+            return -1;
+        }
+    }
+
     
     if (rank == 0) {
+        printf("%s\n", outFileName);
         free(edgeArray);        //  No longer needed
         for (int i = 0; i < numV * numV; i++) {
             printf("%d ", distances[i]);
         }
         printf("\n");
     }
-   
+    
+    free(distances);
     MPI_Finalize();
     return 0;
 }
